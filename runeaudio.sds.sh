ca83693bb3643a1ae15c0b1e562e849ef45ccdb8
audiophonics <contact@audiophonics.fr>
2016-04-13 17:18:32 +0200

    Create runeaudio.sds.sh
    Power management script for RuneAudio

---
+++ runeaudio.sds.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#Thanks to Hondagx35
+#http://www.runeaudio.com/forum/audiophonics-i-sabre-v3-dac-es9023-tcxo-t3377.html#p13106
+#http://www.runeaudio.com/forum/audiophonics-i-sabre-v3-dac-es9023-tcxo-t3377-20.html#p13195
+
+PATH=/bin:/usr/bin:/home/pi/wiringPi/gpio
+
+echo "Audiophonics Shutdown script starting..."
+echo "Asserting pins : "
+echo "ShutDown : GPIO17=in, Low"
+echo "BootOK : GPIO22=out, High"
+
+gpio -g mode 17 in
+gpio -g write 17 0
+gpio -g mode 22 out
+gpio -g write 22 1
+
+while [ 1 ]; do
+if [ "$(/usr/local/bin/gpio -g read 17)" = "1" ]; then
+echo "ShutDown order received, RaspBerry pi will now enter in standby mode..."
+
+/sbin/shutdown -h -t5 now
+break
+fi
+/bin/sleep 0.25
+done
+
+exit 0
