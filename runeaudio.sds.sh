53e0b6e6bbf2668b1d6cc38b8c63254399fdf865
audiophonics <contact@audiophonics.fr>
2016-04-13 19:32:36 +0200

    Update runeaudio.sds.sh
    Need "rune_shutdown poweroff" command to get soft shutdown working

--- runeaudio.sds.sh
+++ runeaudio.sds.sh
@@ -16,12 +16,12 @@ gpio -g mode 22 out
 gpio -g write 22 1
 
 while [ 1 ]; do
-if [ "$(/usr/bin/gpio -g read 17)" = "1" ]; then
-echo "ShutDown order received, RaspBerry pi will now enter in standby mode..."
-
-/sbin/shutdown -h -t5 now
-break
-fi
+  if [ "$(/usr/bin/gpio -g read 17)" = "1" ]; then
+        echo "ShutDown order received, RaspBerry pi will now enter in standby mode..."
+        /var/www/command/rune_shutdown poweroff
+        systemctl poweroff
+        break
+  fi
 /bin/sleep 0.25
 done
 
