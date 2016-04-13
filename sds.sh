bf6cd6424584f40f9ec0bf59934f8f9e266f9883
audiophonics <contact@audiophonics.fr>
2016-04-13 19:27:59 +0200

    Update sds.sh
    Need "rune_shutdown poweroff" command to get soft shutdown working

--- sds.sh
+++ sds.sh
@@ -14,9 +14,10 @@ gpio -g mode 22 out
 gpio -g write 22 1
 
 while [ 1 ]; do
-  if [ "$(/home/pi/wiringPi/gpio/gpio -g read 17)" = "1" ]; then
+  if [ "$(/usr/bin/gpio -g read 17)" = "1" ]; then
         echo "ShutDown order received, RaspBerry pi will now enter in standby mode..."
-        sudo shutdown -h -P now
+        /var/www/command/rune_shutdown poweroff
+        systemctl poweroff
         break
   fi
   /bin/sleep 0.25
